e9f969f7186d585ffeeca388552830d1ca869d9d
Brandon Eduardo Jeronimo Morales <isc18350316@gmail.com>
2022-06-22 21:40:36 -0500

    Pokedex - PokeAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Card from './pokemons/Card';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//recibirá como parámetro el punto final
+export async function getAllPokemon(url) {
+  return new Promise((resolve) => {
+      fetch(url)
+          .then(res => res.json())
+          .then(data => {
+              resolve(data);
+          })
+  })
 }
+export async function getPokemon(url) {
+  return new Promise((resolve) => {
+      fetch(url)
+          .then(res => res.json())
+          .then(data => {
+              resolve(data);
+          })
+  })
+}
+function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [previousUrl, setPreviousUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon?limit=8';
 
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl); 
+      setNextUrl(response.next); 
+      setPreviousUrl(response.previous); 
+      let pokemon = await loadingPokemon(response.results); 
+      console.log(pokemon);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+  
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPreviousUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!previousUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(previousUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPreviousUrl(data.previous);
+    setLoading(false);
+  }
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+        let pokemonRecord = await getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+  // console.log(pokemonData);
+  return (
+    <>
+      { loading ? <h1>Loading...</h1> : (
+        <>
+          <div className="grid-container">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon}/>
+            })}
+          </div>
+          <div className="btn">
+            <button onClick={prev}>←</button>
+            <button onClick={next}>→</button>
+          </div>
+        </>
+      )}
+      
+    </>
+  ); 
+}
 export default App;
---
+++ src/pokemons/Card/Card.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './style.css';
+
+const pokemonColors = {
+    bug: '#729f3f',
+    dragon: '#53a4cf',
+    fairy: '#fdb9e9',
+    fire: '#fd7d24',
+    ghost: '#7b62a3',
+    ground: '#f7de3f',
+    normal: '#a4acaf',
+    pyschic: '#f366b9',
+    steel: '#9eb7b',
+    dark: '#707070',
+    electric: '#eed535',
+    fighting: '#d56723',
+    flying: '#3dc7ef',
+    grass: '#9bcc50',
+    ice: '#51c4e7',
+    poison: '#b97fc9',
+    rock: '#a38c21',
+    water: '#4592c4'
+}
+
+function Card({ pokemon }) {
+    return (
+        <div className="Card">
+            <div className="Card__img">
+                <img src={pokemon.sprites.front_default} alt="" />
+            </div>
+            <div className="Card__name">
+                {pokemon.name}
+            </div>
+            <div className="Card__types">
+                {
+                    pokemon.types.map(type => {
+                        return (
+                            <div className="Card__type" 
+                            style={{ backgroundColor: pokemonColors[type.type.name] }}>
+                                {type.type.name}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="Card__info">
+            <div className="Card__data">
+                    <p className="title">Id = {pokemon.id}</p>   
+                </div>
+                <div className="Card__data">
+                    <p className="title">Experience = {pokemon.base_experience}</p>
+                </div>
+                <div className="Card__data">
+                    <p className="title">Hp</p>
+                    <p className="base_stat">{pokemon.stats[0].base_stat}</p>
+                </div>
+                <div className="Card__data col-sm-6">
+                    <p className="title">Weight</p>
+                    <p>{pokemon.weight}</p>
+                </div>
+                <div className="Card__data">
+                    <p className="title">Height</p>
+                    <p>{pokemon.height}</p>
+                </div>
+                <div className="Card__data">
+                    <p className="title">Ability</p>
+                    <p className="ability">{pokemon.abilities[0].ability.name}</p>
+                </div>
+            </div>
+        </div>      
+    );
+}
+export default Card;
